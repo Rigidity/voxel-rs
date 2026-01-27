163c88255632f5d7c8e73e88901e0f306adf821b
Rigidity <me@rigidnetwork.com>
2026-01-26 23:09:15 -0500

    Teleport to safety

--- src/game_state.rs
+++ src/game_state.rs
@@ -13,11 +13,12 @@ pub struct GameState {
     pub projection: Projection,
     pub world: World,
     pub registry: Arc<Registry>,
+    pub teleported_to_safety: bool,
 }
 
 impl GameState {
     pub fn new(texture_builder: Option<&mut TextureArrayBuilder>) -> Self {
-        let player = Player::new(Vec3::new(8.0, 100.0, 8.0), Vec3::new(0.6, 1.8, 0.6), 1.4);
+        let player = Player::new(Vec3::new(0.0, 0.0, 0.0), Vec3::new(0.6, 1.8, 0.6), 1.4);
         let camera = Camera::new(player.camera_position(), player.yaw, player.pitch);
         let projection = Projection::new(0, 0, 75.0, 0.1, 1000.0);
         let world = World::new(WorldGenerator::new());
@@ -40,6 +41,7 @@ impl GameState {
             projection,
             world,
             registry: Arc::new(registry),
+            teleported_to_safety: false,
         }
     }
 
@@ -56,5 +58,33 @@ impl GameState {
         self.camera.position = self.player.camera_position();
         self.camera.yaw = self.player.yaw;
         self.camera.pitch = self.player.pitch;
+
+        if !self.teleported_to_safety {
+            let mut y = 0;
+
+            loop {
+                let world_pos = IVec3::new(0, y, 0);
+                let chunk_pos = World::chunk_pos(world_pos);
+
+                if !self.world.is_visible_chunk(chunk_pos) {
+                    self.teleported_to_safety = true;
+                    break;
+                }
+
+                if !self.world.chunks.contains_key(&chunk_pos) {
+                    break;
+                }
+
+                if self.world.get_block(world_pos).is_none()
+                    && self.world.get_block(world_pos + IVec3::Y).is_none()
+                {
+                    self.player.position = Vec3::new(0.0, y as f32 + 1.0, 0.0);
+                    self.teleported_to_safety = true;
+                    break;
+                }
+
+                y += 1;
+            }
+        }
     }
 }
